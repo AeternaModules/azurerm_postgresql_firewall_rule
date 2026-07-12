15535b5ae5d4dd960da3ee338679d61dbd1ece34
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:41 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "postgresql_firewall_rules" {
-  description = "All postgresql_firewall_rule resources"
-  value       = azurerm_postgresql_firewall_rule.postgresql_firewall_rules
-}
 output "postgresql_firewall_rules_end_ip_address" {
-  description = "List of end_ip_address values across all postgresql_firewall_rules"
-  value       = [for k, v in azurerm_postgresql_firewall_rule.postgresql_firewall_rules : v.end_ip_address]
+  description = "Map of end_ip_address values across all postgresql_firewall_rules, keyed the same as var.postgresql_firewall_rules"
+  value       = { for k, v in azurerm_postgresql_firewall_rule.postgresql_firewall_rules : k => v.end_ip_address }
 }
 output "postgresql_firewall_rules_name" {
-  description = "List of name values across all postgresql_firewall_rules"
-  value       = [for k, v in azurerm_postgresql_firewall_rule.postgresql_firewall_rules : v.name]
+  description = "Map of name values across all postgresql_firewall_rules, keyed the same as var.postgresql_firewall_rules"
+  value       = { for k, v in azurerm_postgresql_firewall_rule.postgresql_firewall_rules : k => v.name }
 }
 output "postgresql_firewall_rules_resource_group_name" {
-  description = "List of resource_group_name values across all postgresql_firewall_rules"
-  value       = [for k, v in azurerm_postgresql_firewall_rule.postgresql_firewall_rules : v.resource_group_name]
+  description = "Map of resource_group_name values across all postgresql_firewall_rules, keyed the same as var.postgresql_firewall_rules"
+  value       = { for k, v in azurerm_postgresql_firewall_rule.postgresql_firewall_rules : k => v.resource_group_name }
 }
 output "postgresql_firewall_rules_server_name" {
-  description = "List of server_name values across all postgresql_firewall_rules"
-  value       = [for k, v in azurerm_postgresql_firewall_rule.postgresql_firewall_rules : v.server_name]
+  description = "Map of server_name values across all postgresql_firewall_rules, keyed the same as var.postgresql_firewall_rules"
+  value       = { for k, v in azurerm_postgresql_firewall_rule.postgresql_firewall_rules : k => v.server_name }
 }
 output "postgresql_firewall_rules_start_ip_address" {
-  description = "List of start_ip_address values across all postgresql_firewall_rules"
-  value       = [for k, v in azurerm_postgresql_firewall_rule.postgresql_firewall_rules : v.start_ip_address]
+  description = "Map of start_ip_address values across all postgresql_firewall_rules, keyed the same as var.postgresql_firewall_rules"
+  value       = { for k, v in azurerm_postgresql_firewall_rule.postgresql_firewall_rules : k => v.start_ip_address }
 }
 
